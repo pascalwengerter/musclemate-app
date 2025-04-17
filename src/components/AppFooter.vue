f1c527f6a9a8e004558f86c249f2891eee61ba54
Pascal Wengerter <pascal@wengerter.info>
2025-04-17 18:17:49 +0100

    Fix year and broken link in app footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,19 +4,23 @@
       <span
         class="md:float-left md:my-0 md:text-initial md:inline my-3 float-none text-center block"
       >
-        © 2023 MuscleMate
+        © {{currentYear}} MuscleMate
       </span>
       <span
         class="md:float-right md:my-0 md:text-initial md:inline my-3 float-none text-center block lba"
       >
         Design by
-        <a href="https://lowbudgetagency.com" target="_blank" rel="noopener">
-          lowbudgetagency.com
+        <a href="https://www.linkedin.com/in/valentin-eiber-018383173/" target="_blank" rel="noopener">
+          Valentin Eiber
         </a>
       </span>
     </p>
   </footer>
 </template>
+
+<script setup lang="ts">
+const currentYear = new Date().getFullYear();
+</script>
 
 <style>
 .lba a,
