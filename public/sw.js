a7fc0a93a3683a14d1b89be6e5f44378b8a9081c
Pascal Wengerter <pascal@wengerter.info>
2025-11-28 15:54:37 +0100

    Add PWA support

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+// This file is auto-generated for PWA support by VitePWA plugin.
+// You can customize this if you want to add custom service worker logic.
+
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", () => self.clients.claim());
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "MuscleMate",
+        short_name: "MuscleMate",
+        theme_color: "#e5e5e5",
+        background_color: "#e5e5e5",
+        display: "standalone",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
